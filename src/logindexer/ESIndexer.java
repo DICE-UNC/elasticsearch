c53bd37781b76847a875e68610c8ff36c4fbc660
Hao Xu <xuh@email.unc.edu>
2014-07-07 19:01:29 -0400

    delete extra character

--- src/logindexer/ESIndexer.java
+++ src/logindexer/ESIndexer.java
@@ -157,7 +157,7 @@ public class ESIndexer implements Indexer {
 
 			prePropObjForIndexing(o);
 			DeleteByQueryResponse response = client
-g					.prepareDeleteByQuery("databook")
+					.prepareDeleteByQuery("databook")
 					.setQuery(termQuery("uri", o.getUri().toString()))
 					.execute().actionGet();
 		}
